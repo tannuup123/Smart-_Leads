13b3e58d863df0a8c05a7e1ff8f6562ebf248ab9
Tanmay Upadhyay <tanmayup123@gmail.com>
2026-05-17 12:34:16 +0530

    fix: update CORS for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: (origin, callback) => callback(null, true), // Allow all origins for seamless deployment
     credentials: true,
   })
 );
